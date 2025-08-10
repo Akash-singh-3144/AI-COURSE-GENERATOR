12a5f442b952a3c8fa472637976a473917908307
Akash Singh <akashsingh914208@gmail.com>
2025-08-10 16:15:25 +0530

    sync\

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -2,9 +2,7 @@ import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 import React from 'react'
-
 function Header() {
-  
   return (
     <div className='flex justify-between p-5 shadow-sm'>
         <Image src={'/seedofocode_logo.png'} alt='Logo' width={150} height={100}/>
